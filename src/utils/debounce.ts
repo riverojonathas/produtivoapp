90a98f47de2766abad216cb34ae2b2826970c62c
Seu Nome <seu.email@exemplo.com>
2024-11-20 23:51:00 -0300

    Descreva o que foi feito

--- src/utils/debounce.ts
+++ src/utils/debounce.ts
@@ -1,19 +1,16 @@
-export function debounce<T extends (...args: any[]) => any>(
+export function debounce<T extends (...args: unknown[]) => unknown>(
   func: T,
   wait: number
 ): (...args: Parameters<T>) => void {
   let timeout: NodeJS.Timeout | null = null
 
-  return function executedFunction(...args: Parameters<T>) {
-    const later = () => {
-      timeout = null
-      func(...args)
-    }
-
+  return (...args: Parameters<T>) => {
     if (timeout) {
       clearTimeout(timeout)
     }
 
-    timeout = setTimeout(later, wait)
+    timeout = setTimeout(() => {
+      func(...args)
+    }, wait)
   }
-} 
+}
